f87fbd8f01110e0b748b171706be430fddf86c2f
CHARLY <charlyplazas0621@gmail.com>
2022-09-20 17:44:57 -0500

    se optimiza el codigo

---
+++ src/helpers/contantesGET.js
@@ -0,0 +1,7 @@
+const TOKEN =
+    "Bearer BQBK3B4d-L-njmilfG4gSLj04h8-r3ImqTEHDYlOLrjZDXp0kRPpJlTzYDSzPD0M0jVuxUbvHZ3tfsw_hO74p3YkhkzXB619avouj6ahj4O6XwAdvsCBVn0vcI5Y1cYJ7GTuzo_OnbN-hOz6X5EPz6zseHMntrQDkbC0sAlO9cAs";
+
+    export const PETICION={
+        method:"GET",
+        headers:{Authorization:TOKEN}
+    }
